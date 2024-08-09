6bd50b2861748c5db989834faca880fa23fddb4d
Afranio Dantas <afranio.dantas@livsaude.com.br>
2024-08-09 09:53:53 -0300

    temp: creation route post

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 const express = require("express");
+const { v4 } = request("uuid");
+
+app.use(express.json());
 
 const app = express();
 
-app.get("/courses", (request, response) => {
-  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
-});
-
-app.post("/courses", (request, response) => {
-  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.put("/courses/:id", (request, response) => {
-  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.patch("/courses/:id", (request, response) => {
-  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
-});
-
-app.delete("/courses/:id", (request, response) => {
-  return response.json(["Curso 6", "Curso 2", "Curso 4"]);
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
 });
 
 app.listen(3333);
